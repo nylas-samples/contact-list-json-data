24168a9086eaea00c09072bd9f3aa199da9fd8d4
Arjun Kalburgi <askalburgi@gmail.com>
2022-04-08 17:50:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Contact List External Data Svelte
+
+This uses demo data. Create a new component and replace your Component ID to get started.
+
+## Documentation
+
+Learn more about [Nylas Components](https://developer.nylas.com/docs/user-experience/components/).
+
+## Components
+
+Check out the [Components repo](https://github.com/nylas/components).
---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import ContactList from "@nylas/components-contact-list";
+  const staticContacts = [
+    {
+      emails: [
+        {
+          email: "tom@brightideas.com"
+        }
+      ],
+      given_name: "Thomas Edison"
+    },
+    {
+      emails: [
+        {
+          email: "alex@bell.com"
+        }
+      ],
+      given_name: "Alexander Graham Bell"
+    },
+    {
+      emails: [
+        {
+          email: "al@particletech.com"
+        }
+      ],
+      given_name: "Albert Einstein"
+    }
+  ];
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    height: 800px;
+  }
+</style>
+
+<main>
+	<h1>Nylas Contact List</h1>
+  <nylas-contact-list contacts={staticContacts}/>
+</main>
